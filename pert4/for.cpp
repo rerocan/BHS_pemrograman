98b402d18797adf8c7e93af204b1ca0378db622c
rerocan <setyabima181@gmail.com>
2024-04-24 15:17:12 +0700

    update

---
+++ pert4/for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    cout << "Iteration Statment" << endl;
+    cout << "Penggunaan for" << endl;
+
+    for (int a = 1; a <= 5; a++){
+        cout << a << endl;
+    }
+    return 0;
+}
